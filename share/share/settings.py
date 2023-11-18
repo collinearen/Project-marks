8e00285ff5911e7e271b3946cd27e604aaad163d
Danya <118291046+collinearen@users.noreply.github.com>
2023-11-18 14:23:14 +0300

    v5.0.2.8

--- share/share/settings.py
+++ share/share/settings.py
@@ -10,7 +10,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
